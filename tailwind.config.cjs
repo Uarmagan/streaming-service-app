ccfd3a0b3071786c6a68cce07daae33c0ca3295c
Ugur Armagan <ugurarmagan93@gmail.com>
2023-01-13 12:22:29 -0800

    feature: adding tailwind configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit: ["outfit-regular", "sans-serif"],
+        outfitLight: ["outfit-light", "sans-serif"],
+        outfitMedium: ["outfit-medium", "sans-serif"],
+      },
+      fontSize: {
+        headingLg: "2rem",
+        headingMd: "1.5rem",
+        headingSm: "1.5rem",
+        headingXs: "1.125px",
+        bodyMd: "0.9735",
+        bodySm: "0.8125",
+      },
+    },
+    colors: {
+      red: "#FC4747",
+      darkBlue: "#10141E",
+      greyishBlue: "#5A698F",
+      semiDarkBlue: "#161D2F",
+      white: "#FFFFFF",
+    },
   },
   plugins: [],
 };
